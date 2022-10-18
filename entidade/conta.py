bbfd44a183d90af27160ae7da836c31ad406d337
Pedro Schuster <pedro.ssc42@gmail.com>
2022-10-18 04:32:31 -0300

    Criação das Entidades:conta e operacao

--- entidade/conta.py
+++ entidade/conta.py
@@ -1,4 +1,5 @@
 class Conta:
+    
     def __init__(self, numero, tipo, cliente, funcionario):
         self.__numero = numero
         self.__saldo = 0
@@ -7,38 +8,38 @@ class Conta:
         self.__funcionario = funcionario
         self.operacoes = []
         
-        @property 
-        def numero(self):
-            return self.__numero
+    @property 
+    def numero(self):
+        return self.__numero
         
-        @numero.setter
-        def numero(self, novo_numero):
-            self.__numero = novo_numero
+     @numero.setter
+    def numero(self, novo_numero):
+        self.__numero = novo_numero
             
-        @property
-        def saldo(self):
-            return self.__saldo
+    @property
+    def saldo(self):
+        return self.__saldo
         
-        @saldo.setter
-        def saldo(self, valor):
-            self.__saldo = valor
+    @saldo.setter
+    def saldo(self, valor):
+        self.__saldo = valor
         
-        @property
-        def tipo(self):
-            return self.__tipo
+    @property
+    def tipo(self):
+        return self.__tipo
         
-        @tipo.setter
-        def tipo(self, tipo):
-            self.__tipo = tipo
+    @tipo.setter
+    def tipo(self, tipo):
+        self.__tipo = tipo
         
-        @property
-        def cliente(self):
-            return self.__cliente
+    @property
+    def cliente(self):
+        return self.__cliente
         
-        @property
-        def funcionario(self):
-            return self.__funcionario
+    @property
+    def funcionario(self):
+        return self.__funcionario
         
-        @funcionario.setter
-        def funcionario(self, funcionario):
-            self.__funcionario = funcionario
+    @funcionario.setter
+    def funcionario(self, funcionario):
+        self.__funcionario = funcionario
